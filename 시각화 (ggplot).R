adb4cfc225168063b6d39256db1487915094e046
hena7932 <hanna793237@gmail.com>
2020-11-02 21:08:52 +0900

    commit

---
+++ 시각화 (ggplot).R
@@ -0,0 +1,84 @@
+library(tibble)
+library(readr)
+library(readxl)
+library(lubridate)
+library(ggplot2)
+library(tidyverse)
+library(dplyr)
+library(stringr)
+sessionInfo()
+
+#지역별로 확인하는 사업체와 종사자
+
+readr::guess_encoding("data/청년기업.csv",  n_max = 100) # 대표자 정보 청년
+
+young <- read.csv("data/청년기업.csv", fileEncoding = "UTF-8")
+
+glimpse(young)
+
+# 데이터 전처리
+
+young_a <- select(young, - (등록일자:작업자명), - (업종중분류코드:업력구간코드))
+
+glimpse(young_a)
+
+# 시각화
+col = count(young_a, 업종대분류코드)
+
+# 업종별 (내림차순) 
+col = count(young_a, 업종대분류코드)
+
+col %>%
+  ggplot(aes(x = reorder(업종대분류코드, -n), y = n)) +
+  geom_bar(stat = "identity", position= 'dodge', width=.5, fill= "#B66DFF") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(size = 10, face = "bold"),
+        axis.text.y = element_text(size =10, face = 'bold'),
+        axis.title=element_text(size=10, face='bold'),
+        title = element_text(size=10, face='bold'),
+        legend.position = "top") +
+  labs(title="업종대분류")
+
+# 가로형
+ggplot(data = col, aes(x = reorder(업종대분류코드, -n), y = n)) + geom_col(aes(colour=업종대분류코드)) + coord_flip()
+
+# 연령대 코드
+age = count(young_a, 연령대코드)
+age %>%
+  filter(연령대코드 %in% c("20", "30", "40", "50", "60", "70")) %>% 
+  ggplot(aes(x = 연령대코드, y = n)) +
+  geom_bar(stat = "identity", position= 'dodge', width=.5, fill= "#FF6666") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(size = 10, face = "bold"),
+        axis.text.y = element_text(size =10, face = 'bold'),
+        axis.title=element_text(size=10, face='bold'),
+        title = element_text(size=10, face='bold'),
+        legend.position = "top") +
+  labs(title="연령대")
+
+# 시도명 코드
+SD = count(young_a, 시도명)
+SD %>%
+ggplot(aes(x = reorder(시도명, -n), y = n)) +
+  geom_bar(stat = "identity", position= 'dodge', width=.5, fill= "#00B2D9") +
+  coord_flip() + # 가로형코드
+  theme_minimal() +
+  theme(axis.text.x = element_text(size = 10, face = "bold"),
+        axis.text.y = element_text(size =10, face = 'bold'),
+        axis.title=element_text(size=10, face='bold'),
+        title = element_text(size=10, face='bold'),
+        legend.position = "top") +
+  labs(title="시도")
+
+
+SD %>%
+  filter(시도명 %in% c("서울", "경기", "부산", "경남&경북", "전남&전북", "충북&충남", "인천")) %>% 
+  ggplot(aes(x = 시도명, y = n)) +
+  geom_bar(stat = "identity", position= 'dodge', width=.5, fill= "#FF6666") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(size = 10, face = "bold"),
+        axis.text.y = element_text(size =10, face = 'bold'),
+        axis.title=element_text(size=10, face='bold'),
+        title = element_text(size=10, face='bold'),
+        legend.position = "top") +
+  labs(title="시도명")
